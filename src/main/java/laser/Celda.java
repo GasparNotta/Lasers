1dc75227e3267b95cde8a90c0eb29b5255c888c0
GasparNotta <gasparnotta11@gmail.com>
2024-09-20 14:29:23 -0300

    Class Celda

--- src/main/java/laser/Celda.java
+++ src/main/java/laser/Celda.java
@@ -1,6 +1,28 @@
 package laser;
 
 public class Celda {
-    
+    private boolean tienePiso;
+    private Bloque bloque;
+
+    public Celda(boolean tienePiso) {
+        this.tienePiso = tienePiso;
+        this.bloque = null; // Celda vacía inicialmente
+    }
+
+    public boolean tienePiso() {
+        return tienePiso;
+    }
+
+    public Bloque getBloque() {
+        return bloque;
+    }
+
+    public void setBloque(Bloque bloque) {
+        this.bloque = bloque;
+    }
+
+    public boolean estaVacia() {
+        return tienePiso && bloque == null;
+    }
 }
 
