55b3d64db62729f90f396a67cd543072388428cb
GasparNotta <gasparnotta11@gmail.com>
2024-09-20 14:29:11 -0300

    Class Cordenada

---
+++ src/main/java/laser/Coordenada.java
@@ -0,0 +1,34 @@
+package laser;
+
+public class Coordenada {
+    private int x;
+    private int y;
+
+    public Coordenada(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // Método para verificar si la coordenada es impar/impar (para bloques)
+    public boolean esBloque() {
+        return x % 2 != 0 && y % 2 != 0;
+    }
+
+    // Método para verificar si la coordenada es par/impar o impar/par (para emisores/objetivos)
+    public boolean esBorde() {
+        return (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+    }
+}
