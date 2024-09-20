45f9fa392a67f51d7888b6f48f116e1a2fb717d1
GasparNotta <gasparnotta11@gmail.com>
2024-09-20 14:55:32 -0300

    Class Laser

--- src/main/java/laser/Laser.java
+++ src/main/java/laser/Laser.java
@@ -1,5 +1,40 @@
 package laser;
 
 public class Laser {
+    private int x;  // Coordenada X
+    private int y;  // Coordenada Y
+    private int direccion;  // Dirección del rayo láser
+
+    public Laser(int x, int y, int direccion) {
+        this.x = x;
+        this.y = y;
+        this.direccion = direccion;
+    }
+
+    // Getters y setters de las coordenadas
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Getters y setters de la dirección
+    public int getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(int direccion) {
+        this.direccion = direccion;
+    }
     
 }
