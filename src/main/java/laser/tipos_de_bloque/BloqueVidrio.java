e16a0de911ebf1fe53c3fdc8bb0c25379bb0e68f
GasparNotta <gasparnotta11@gmail.com>
2024-09-22 02:58:14 -0300

    Class BloqueVidrio Version 2

--- src/main/java/laser/tipos_de_bloque/BloqueVidrio.java
+++ src/main/java/laser/tipos_de_bloque/BloqueVidrio.java
@@ -1,5 +1,17 @@
 package laser.tipos_de_bloque;
+import laser.Laser;
+import laser.Bloque;
 
-public class BloqueVidrio {
-    
+public class BloqueVidrio extends Bloque {
+    public BloqueVidrio() {
+        super("G"); // Tipo de bloque
+    }
+
+    @Override
+    public void interactuarConLaser(Laser laser) {
+        // Difracta el rayo en 2
+        laser.reflejar();
+        laser.difractar();
+        System.out.println("Bloque de Vidrio: El rayo se ha difractado.");
+    }
 }
