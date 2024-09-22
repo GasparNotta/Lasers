16dca89c9d8620e4239823f2e9f14a232c5fbd9b
GasparNotta <gasparnotta11@gmail.com>
2024-09-22 02:56:51 -0300

    Class BloqueMovil Version 2

--- src/main/java/laser/tipos_de_bloque/BloqueMovil.java
+++ src/main/java/laser/tipos_de_bloque/BloqueMovil.java
@@ -1,5 +1,15 @@
 package laser.tipos_de_bloque;
+import laser.Laser;
+import laser.Bloque;
 
-public class BloqueMovil {
-    
+public class BloqueMovil extends Bloque {
+    public BloqueMovil() {
+        super("B"); // Tipo de bloque
+    }
+
+    @Override
+    public void interactuarConLaser(Laser laser) {
+        // Absorbe rayos láser, no hace nada
+        System.out.println("Bloque Opaco Móvil: El rayo ha sido absorbido.");
+    }
 }
