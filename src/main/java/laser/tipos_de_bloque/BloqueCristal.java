872b3494682ef5b57c93b40d60a47a82b53f6e3f
GasparNotta <gasparnotta11@gmail.com>
2024-09-22 02:58:42 -0300

    Class BloqueCristal Version 2

--- src/main/java/laser/tipos_de_bloque/BloqueCristal.java
+++ src/main/java/laser/tipos_de_bloque/BloqueCristal.java
@@ -1,5 +1,16 @@
 package laser.tipos_de_bloque;
+import laser.Laser;
+import laser.Bloque;
 
-public class BloqueCristal{
-    
+public class BloqueCristal extends Bloque {
+    public BloqueCristal() {
+        super("C"); // Tipo de bloque
+    }
+
+    @Override
+    public void interactuarConLaser(Laser laser) {
+        // Refracta el rayo, continuando en línea recta
+        laser.refractar();
+        System.out.println("Bloque de Cristal: El rayo se ha refractado.");
+    }
 }
