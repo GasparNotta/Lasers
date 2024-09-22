6141de5092a70f98baa5c302b3685365ec6112d3
GasparNotta <gasparnotta11@gmail.com>
2024-09-22 02:55:47 -0300

    Class BloqueFijo Version 2

--- src/main/java/laser/tipos_de_bloque/BloqueFijo.java
+++ src/main/java/laser/tipos_de_bloque/BloqueFijo.java
@@ -1,5 +1,15 @@
 package laser.tipos_de_bloque;
+import laser.Laser;
+import laser.Bloque;
 
-public class BloqueFijo {
-    
+public class BloqueFijo extends Bloque {
+    public BloqueFijo() {
+        super("F"); // Tipo de bloque
+    }
+
+    @Override
+    public void interactuarConLaser(Laser laser) {
+        // Absorbe rayos láser, no hace nada
+        System.out.println("Bloque Opaco Fijo: El rayo ha sido absorbido.");
+    }
 }
